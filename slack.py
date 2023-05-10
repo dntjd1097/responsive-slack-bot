66bc1b24539281648a900f0439ec36aabab719f5
WOOSEONG JUNG | JAMES <46855318+dntjd1097@users.noreply.github.com>
2023-05-10 16:24:44 +0900

    다시 (#10)
    * Delete .github/workflows directory
    
    * refactor : 주석 제거
    
    * button delete 구현

--- slack.py
+++ slack.py
@@ -5,6 +5,8 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import asyncio
 import html
+import re
+from datetime import datetime, timezone, timedelta
 
 try:
     from config import *
@@ -17,8 +19,6 @@ except ModuleNotFoundError:
 client = WebClient(token=bot_token)
 app = App(token=bot_token)
 
-# Define the message to send
-
 
 def update(channel_id, message_ts, text, new_text):
     response = client.chat_update(
@@ -30,16 +30,15 @@ def update(channel_id, message_ts, text, new_text):
 
 
 async def get_message(payload, a):
+    today = datetime.now(timezone(timedelta(hours=9))).strftime("%Y-%m-%d")
     channel_id = payload["channel"]["id"]
-
     user_id = payload["user"]["id"]
     user_name = payload["user"]["name"]
     message_ts = payload["container"]["message_ts"]
     text = payload["message"]["text"]
     comment = f"<@{user_id}>, "
     data = payload["message"]["blocks"]
-    # print(data)
-    context_block_index = None
+    get_date = ""
     if a == 1:
         status = "*참석*\n "
         op_status = "*불참*\n "
@@ -57,33 +56,21 @@ async def get_message(payload, a):
             {"type": "mrkdwn", "text": "*불참인원*\n 0 *(명)*"},
         ],
     }
-    # print(data)
+
     if "divider" in data[-2]["type"]:
-        # print(data[-2])
         data.insert(-1, insert)
-    for block in data:
+
+    for index, block in enumerate(data):
         try:
-            # print(block)
-            # 참석 클릭 시
-            # print(block)
-            # print(block)
+            if "context" in block["type"]:
+                get_date = block["elements"][0]["text"]
+                pattern = r"\d{4}-\d{2}-\d{2}"
+                matches = re.findall(pattern, get_date)
+                get_date = matches[0]
+            if ("actions" in block["type"]) and get_date and get_date < today:
+                del data[index]
+                return
             if "section" in block["type"]:
-                # print(block["fields"])
-                """
-                if op_status in block["fields"][1]["text"]:
-                    a = block["fields"][1]["text"]
-                    a = a.replace(comment, "")
-                    for person in data[-2]["fields"]:
-                        if person["text"].find(op_ps_status) == 0:
-                            person["text"] = person["text"].split()
-                            person["text"][0] += "\n"
-                            if int(person["text"][1]) > 0:
-                                person["text"][1] = str(int(person["text"][1]) - 1)
-
-                            person["text"] = " ".join(person["text"])
-                    print(block["fields"][0])
-                """
-
                 for a in block["fields"]:
                     if op_status in a["text"]:
                         a["text"] = a["text"].replace(comment, "")
@@ -105,46 +92,10 @@ async def get_message(payload, a):
                             for person in data[-2]["fields"]:
                                 if person["text"].find(ps_status) == 0:
                                     person["text"] = person["text"].split()
-                                    # print(person["text"])
                                     person["text"][0] += "\n"
-
                                     person["text"][1] = str(a["text"].count("@"))
                                     person["text"] = " ".join(person["text"])
-                # data.append(insert)
-            """
-            if op_status in block["elements"][0]["text"]:
-                if block["elements"][0]["text"].find(comment) > 0:
-                    block["elements"][0]["text"] = block["elements"][0]["text"].replace(
-                        comment, ""
-                    )
 
-                    for person in data[-2]["fields"]:
-                        if person["text"].find(op_ps_status) == 0:
-                            person["text"] = person["text"].split()
-                            person["text"][0] += "\n"
-                            if int(person["text"][1]) > 0:
-                                person["text"][1] = str(int(person["text"][1]) - 1)
-                                
-
-                            person["text"] = " ".join(person["text"])
-
-            if status in block["elements"][0]["text"]:
-                if comment in block["elements"][0]["text"]:
-                    pass
-                else:
-                    # pass
-                    block["elements"][0]["text"] += comment
-
-                    for person in data[-2]["fields"]:
-                        if person["text"].find(ps_status) == 0:
-                            person["text"] = person["text"].split()
-                            # print(person["text"])
-                            person["text"][0] += "\n"
-
-                            person["text"][1] = str(int(person["text"][1]) + 1)
-                            person["text"] = " ".join(person["text"])
-            # print(block["elements"][0]["text"])
-            """
         except KeyError as e:
             pass
 
@@ -168,20 +119,16 @@ def handle_attent_edit_button_click(ack, body, logger):
     ack()
     logger.info(body)
     asyncio.run(get_message(body, 1))
-    # asyncio.run(attend_edit_message(body))
 
 
 @app.action("nonattend")
 def handle_nonattent_edit_button_click(ack, body, logger):
     ack()
     logger.info(body)
-    # print(logger.info(body))
-    # print(body)
     asyncio.run(get_message(body, 0))
     # asyncio.run(nonattend_edit_message(body))
 
 
-# response = client.chat_postMessage(**message)
 if __name__ == "__main__":
     # Start the bot
     handler = SocketModeHandler(app_token=app_token, app=app)
